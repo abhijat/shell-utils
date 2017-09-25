c46d0653510957e286bbcc0311b1bfefc030dd5b
Abhijat Malviya <malviya.abhijat@gmail.com>
2017-09-25 21:37:31 +0530

    adds ability to run a command after selecting the directory

--- functions.sh
+++ functions.sh
@@ -5,10 +5,37 @@ source "$(dirname ${BASH_SOURCE[0]})"/colors.sh
 JAVA_PROJECTS_ROOT=${HOME}/dev/java
 
 _java_projects() {
+
     local cur=${COMP_WORDS[COMP_CWORD]}
     local files=( ${JAVA_PROJECTS_ROOT}/* )
 
-    COMPREPLY=( $(compgen -W "${files[*]##*/}" -- "$cur") )
+    if [ ${COMP_CWORD} -eq 1 ]; then
+        COMPREPLY=( $(compgen -W "${files[*]##*/}" -- "$cur") )
+    elif [ ${COMP_CWORD} -eq 2 ]; then
+        COMPREPLY=( $(compgen -W "git-status maven-clean-install spring-boot-run" -- "$cur") )
+    fi
+}
+
+run_command() {
+    CMD="$1"
+    if [ -z "${CMD}" ]; then
+        return
+    fi
+
+    case "${CMD}" in
+        "git-status")
+            printf "$GRN" "git status"
+            git status
+            ;;
+        "maven-clean-install")
+            printf "$GRN" "mvn clean install"
+            mvn clean install
+            ;;
+        "spring-boot-run")
+            printf "$GRN" "mvn spring-boot:run"
+            mvn spring-boot:run
+            ;;
+    esac
 }
 
 gobuild() {
@@ -25,9 +52,10 @@ gobuild() {
     cd "${FULL_PATH}" || return 1
 
     printf "%s\n" "$PWD"
-    printf "$BLU" "executing mvn clean install..."
 
-    mvn clean install
+    if [ -n "$2" ]; then
+        run_command "$2"
+    fi
 }
 
 complete -F _java_projects gobuild
